a83ca43713c78cdfd8301b97fd279de10d52a6a5
Sergey Nikontsev <n-sergy@ya.ru>
2022-01-25 00:46:21 +0300

    feat: add hamburger

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,1 +1,8 @@
-console.log ('portfolio');
+console.log ('68.5 баллов за самопроверку');
+
+const hamburger = document.querySelector('.hamburger');
+
+function toggleMenu() {
+  hamburger.classList.toggle('open');
+}
+hamburger.addEventListener('click', toggleMenu);
