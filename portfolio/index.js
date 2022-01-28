0db2c34b12849bfd0c5c50a94085a3c14b662156
Sergey Nikontsev <n-sergy@ya.ru>
2022-01-29 01:35:05 +0300

    feat: add toggle buttons

--- portfolio/index.js
+++ portfolio/index.js
@@ -6,3 +6,13 @@ function toggleMenu() {
   hamburger.classList.toggle('open');
 }
 hamburger.addEventListener('click', toggleMenu);
+
+
+const btnSwitch = document.querySelectorAll('.btn-switch');
+
+function toggleBtn(event) {  
+  btnSwitch.forEach(itemBtn => {itemBtn.classList.remove('active'); });  
+  event.target.classList.add('active'); 
+}
+
+btnSwitch.forEach((item) => {item.addEventListener('click', toggleBtn)});
