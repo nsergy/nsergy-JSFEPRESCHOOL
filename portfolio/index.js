11c6188f99cdd6e8ac797e202df67835ef6cbe23
Sergey Nikontsev <n-sergy@ya.ru>
2022-01-29 02:20:30 +0300

    feat: add image change

--- portfolio/index.js
+++ portfolio/index.js
@@ -9,10 +9,12 @@ hamburger.addEventListener('click', toggleMenu);
 
 
 const btnSwitch = document.querySelectorAll('.btn-switch');
+const portfolioImages = document.querySelectorAll('.portfolio-item');
 
 function toggleBtn(event) {  
   btnSwitch.forEach(itemBtn => {itemBtn.classList.remove('active'); });  
   event.target.classList.add('active'); 
+  portfolioImages.forEach((img, index) => img.src = `./assets/img/${event.target.dataset.season}/${index + 1}.jpg`);
 }
 
 btnSwitch.forEach((item) => {item.addEventListener('click', toggleBtn)});
