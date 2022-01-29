268d3a80d914cb0c5a0816d0f6d79c9d5adda47a
Sergey Nikontsev <n-sergy@ya.ru>
2022-01-30 01:06:31 +0300

    feat: add translate

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,5 +1,7 @@
 console.log ('68.5 баллов за самопроверку');
 
+import i18Obj from './translate.js';
+
 const hamburger = document.querySelector('.hamburger');
 
 function toggleMenu() {
@@ -18,3 +20,27 @@ function toggleBtn(event) {
 }
 
 btnSwitch.forEach((item) => {item.addEventListener('click', toggleBtn)});
+
+
+const langSwitch = document.querySelectorAll('.nav-lang-link');
+
+function toggleLang(event) {
+  langSwitch.forEach(itemLang => {itemLang.classList.remove('active-lang'); });
+  event.target.classList.add('active-lang');
+  getTranslate(event.target.dataset.lang);
+}
+
+langSwitch.forEach((item) => {item.addEventListener('click', toggleLang); });
+
+function getTranslate(lang) {
+  const langValue = document.querySelectorAll('[data-i18n]');
+  langValue.forEach((value) => {
+    value.textContent = i18Obj[lang][value.dataset.i18n]; 
+    if (value.placeholder) {
+      value.placeholder = i18Obj[lang][value.dataset.i18n];
+      value.placeholder = i18Obj[lang][value.dataset.i18n];
+      value.textContent = i18Obj[lang][value.dataset.i18n];
+    }  
+  });
+
+}
