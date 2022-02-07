98f10af05a6cd99f4ee245d2a43c23b2794f7bee
Sergey Nikontsev <n-sergy@ya.ru>
2022-02-07 23:06:05 +0300

    fix: bug fixes, final version

--- eco-sounds/index.js
+++ eco-sounds/index.js
@@ -4,8 +4,16 @@ let isPlay = false;
 let sound = 'forest';
 const audio = new Audio();
 
-const singer = document.querySelectorAll('[data-item]');
 
+window.addEventListener('load', pushLocalStorage);
+
+function pushLocalStorage () {
+    localStorage.setItem('isPlay', isPlay);
+    localStorage.setItem('sound', 'forest');
+    audio.src = `./assets/audio/${localStorage.getItem('sound')}.mp3`;    
+}
+
+const singer = document.querySelectorAll('[data-item]');
 singer.forEach(item => {item.addEventListener('click', toggleSinger)});
 
 function toggleSinger(element) {
@@ -14,25 +22,24 @@ function toggleSinger(element) {
     localStorage.setItem('sound', element.target.dataset.item);
     audio.src = `./assets/audio/${localStorage.getItem('sound')}.mp3`;
     audio.currentTime = 0;
-    audio.play();
-    btnPlayStop.src = "./assets/svg/play.svg";
-    localStorage.setItem('isPlay', true);
+    audio.pause();
     document.getElementById('main').style.backgroundImage = `url('./assets/img/${localStorage.getItem('sound')}.jpg')`;
+    togglePlayer();
 }
 
 const btnPlayStop = document.querySelector('.player-btn');
-
 btnPlayStop.addEventListener('click', togglePlayer);
 
 function togglePlayer() {
-    if (localStorage.getItem('isPlay') === 'true') {
+    if (!audio.paused) {
         localStorage.setItem('isPlay', false);
         audio.pause();
         btnPlayStop.src = "./assets/svg/pause.svg";
-    }
+    } 
     else {
-        localStorage.setItem('isPlay', true);
         audio.play();
         btnPlayStop.src = "./assets/svg/play.svg";
+        localStorage.setItem('isPlay', true);
     }
+
 }
