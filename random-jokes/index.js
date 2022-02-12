dc9ddb00a7e8f4822da270d03d501851b35f17d8
Sergey Nikontsev <n-sergy@ya.ru>
2022-02-13 01:31:49 +0300

    feat: add quotes

--- random-jokes/index.js
+++ random-jokes/index.js
@@ -1,1 +1,33 @@
 console.log("random-jokes");
+
+async function getQuotes() {  
+    const res = await fetch('https://api.icndb.com/jokes/random');  //С сайта
+    //const res = await fetch('./assets/quotes_ru.json');   //Из файла
+    const data = await res.json(); 
+    console.log(data);
+    console.log(data.value.joke); //С сайта
+    //console.log(data[0].text);  //Из файла
+    outputJoke(data.value.joke);
+  }
+  getQuotes();
+
+  
+  function outputJoke(data) {
+    document.querySelector('.jokes').textContent = data;
+  }
+
+  const lang = document.querySelectorAll('.lang-link');
+
+  function toggleLang() {
+      console.log('сброс языка???');
+      lang.forEach(element => {
+          element.classList.remove('active-lang');
+      })
+  }
+
+  lang.forEach(item => {item.addEventListener('click', toggleLang())});
+
+  document.querySelector('.button').addEventListener('click', getQuotes);
+
+
+
